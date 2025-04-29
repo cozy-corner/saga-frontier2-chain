45ea1e4d44a4ec46b85d1f69042d1eed38ba304f
Koji Sasaki <sasakicozy@gmail.com>
2025-04-30 06:33:55 +0900

    add style (#4)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import App from './App';
+import './index.css';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql',
